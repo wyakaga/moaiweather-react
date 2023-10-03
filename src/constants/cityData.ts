cdf8fd013724d4e1be1ed7c5c5db6dc162acd37c
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-10-04 06:07:25 +0700

    [FEAT] Add city data

--- src/constants/cityData.ts
+++ src/constants/cityData.ts
@@ -6,20 +6,40 @@ const cityData: ICityData[] = [
     name: "Jakarta, DKI Jakarta",
   },
   {
+    value: "Surabaya",
+    name: "Surabaya, East Java",
+  },
+  {
     value: "Medan",
     name: "Medan, North Sumatra",
+  },
+  {
+    value: "Palembang",
+    name: "Palembang, South Sumatra",
   },
   {
     value: "Balikpapan",
     name: "Balikpapan, East Kalimantan",
   },
   {
+    value: "Samarinda",
+    name: "Samarinda, East Kalimantan",
+  },
+  {
     value: "Makassar",
     name: "Makassar, South Sulawesi",
   },
   {
+    value: "Manado",
+    name: "Manado, North Sulawesi",
+  },
+  {
     value: "Jayapura",
     name: "Jayapura, Papua",
+  },
+  {
+    value: "Sorong",
+    name: "Sorong, Southwest Papua",
   },
 ];
 
