bf6a6e8d71cfe7a3f77ab85e38e7df0ea6b91fda
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-10-04 06:01:11 +0700

    [FIX] Fix router

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -5,7 +5,7 @@ import pagesData from "./pagesData";
 
 const Router = () => {
 	const pageRoutes = pagesData.map(({ path, title, element }: IRouter) => {
-		return <Route key={title} path={`/${path}`} element={element} />;
+		return <Route key={title} path={`${path}`} element={element} />;
 	});
 
 	return <Routes>{pageRoutes}</Routes>;
